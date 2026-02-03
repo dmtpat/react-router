dd49f1b7d4da161a1d64d54b5398b6e9edfb013d
Pat <dmtpatricica@email.com>
2026-02-03 13:49:10 +0100

    footer style manual

--- src/components/Footer.components.jsx
+++ src/components/Footer.components.jsx
@@ -1,12 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'bootstrap/dist/js/bootstrap.min.js'
 
 function Footer() {
     return (
-        <div className="container-fluid bg-dark-subtle text-center text-dark position-absolute bottom-0">
+        <footer className="full_width flex ms_j_center">
             <p>Questo è il footer</p>
-        </div>
+        </footer>
     )
 }
 export default Footer
