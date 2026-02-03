c77016ee785c029cae6bb75963a11cc541134dac
Pat <dmtpatricica@email.com>
2026-02-03 18:37:29 +0100

    cards + imports from axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react'
 
-import axios from 'axios'
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import DefaultLayout from './layouts/DefaultLayout';
 import Page_home from "./pages/page_home"
--- src/pages/page_prodotti.jsx
+++ src/pages/page_prodotti.jsx
@@ -1,10 +1,35 @@
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function Page_prodotti() {
+    const [prodotti, setProdotti] = useState([]);
+    const apiUrl = "https://fakestoreapi.com/products";
+
+    function getData() {
+        axios.get(apiUrl).then((result) => {
+            setProdotti(result.data);
+            console.log(result.data);
+        }).catch((error) => <p>Ops.. Qualcosa non va</p>)
+    }
+
+    useEffect(getData, []);
+
     return (
-        <>
+        <main className="boxed">
             <h1>Questi sono i nostri prodotti</h1>
-        </>
+            <div className="card_container">
+                {prodotti.map((prodotto) => {
+                    return (
+                        <div className="card" key={prodotto.id}>
+                            <img src={prodotto.image} alt="" />
+                            <div className="marquee">
+                                <h2>{prodotto.title}</h2>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </main>
     )
 }
 export default Page_prodotti
