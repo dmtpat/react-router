627fd6fa07b4b974cb114924b6ad6f6c0863e9fc
Pat <dmtpatricica@email.com>
2026-02-05 18:54:13 +0100

    product detail style + minor

--- src/pages/page_prodotti.jsx
+++ src/pages/page_prodotti.jsx
@@ -21,13 +21,12 @@ function Page_prodotti() {
             <div className="card_container">
                 {prodotti.map((prodotto) => {
                     return (
-                        <div className="card" key={prodotto.id} >
+                        <Link to={`/prodotti/${prodotto.id}`} className="card" key={prodotto.id} >
                             <img src={prodotto.image} alt="" />
                             <div className="marquee">
                                 <h2>{prodotto.title}</h2>
                             </div>
-                            <Link to={`/prodotti/${prodotto.id}`} >Vai ai dettagli</Link>
-                        </div>
+                        </Link>
                     )
                 })}
             </div>
--- src/pages/page_prodotti_detail.jsx
+++ src/pages/page_prodotti_detail.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link, useParams } from "react-router-dom";
-
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function Product_detail() {
     const { id } = useParams();
-
-    const [prodotto, setProdotto] = useState([]);
+    const navigate = useNavigate();
+    const [prodotto, setProdotto] = useState({});
+    const [rates, setRates] = useState({});
     let apiUrl = `https://fakestoreapi.com/products/${parseInt(id)}`;
 
     function getData() {
@@ -14,18 +16,51 @@ function Product_detail() {
         axios.get(apiUrl).then((result) => {
             setProdotto(result.data);
             console.log(result.data);
-        }).catch((error) => <p>Ops.. Qualcosa non va</p>)
+            console.log(result.data.rating);
+            console.log(result.data.rating.rate);
+            setRates(result.data.rating);
+        }).catch((error) => {
+            <p>Ops.. Qualcosa non va</p>
+            navigate("/prodotti");
+        })
     }
 
-    useEffect(getData, []);
+    useEffect(getData, [id]);
+
+    function ratingStars() {
+        const oneStar = <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B", }} />;
+        let stars = oneStar;
+        for (let i = 1; i < rates.rate; i++) {
+            stars += oneStar;
+        }
+        return stars;
+    }
 
     return (
         <main>
-            <h1>{prodotto.title} </h1>
-            <p>{prodotto.category}</p>
-            <img src={prodotto.image} alt={prodotto.title} />
-            <p>{prodotto.description}</p>
-            <p>&euro; {prodotto.price}</p>
+            <div className="boxed details">
+                <h1 className='full_width'>{prodotto.title} </h1>
+                <p className='category' >{prodotto.category}</p>
+                <div className="flex">
+                    <div className="half_width">
+                        <img src={prodotto.image} alt={prodotto.title} />
+                    </div>
+                    <div className="half_width flex details_texts">
+                        <div className="ratings">
+                            {/* {ratingStars()} */}
+                            {rates.rate}
+                            <FontAwesomeIcon icon={faStar} style={{ color: "#FFD43B", }} />
+                            <span> {rates.count}</span>
+                        </div>
+                        <p>{prodotto.description}</p>
+                        <p>&euro; {prodotto.price}</p>
+                    </div>
+                </div>
+                <div className='full_width flex ms_between'>
+                    <button onClick={() => navigate(`/prodotti/${parseInt(id) - 1}`)}>Vai al prodotto precedente</button>
+                    <button onClick={() => navigate(`/prodotti/${parseInt(id) + 1}`)}>Vai al prodotto successivo</button>
+                </div>
+            </div>
         </main>
     )
 }
